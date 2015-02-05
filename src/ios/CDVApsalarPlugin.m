27dc6449b4d069b59e0ceaba8ced6e318c922b73
Christoph Altmann <christoph.altmann@platogo.com>
2015-02-05 10:00:45 +0100

    Add missing semicolons

--- src/ios/CDVApsalarPlugin.m
+++ src/ios/CDVApsalarPlugin.m
@@ -12,12 +12,12 @@
 
 - (void)sendEvent:(CDVInvokedUrlCommand*) command
 {
-    NSString *eventName = [command.arguments objectAtIndex:0]
-    NSString *key = [command.arguments objectAtIndex:1]
-    NSString *value = [command.arguments objectAtIndex:2]
+    NSString *eventName = [command.arguments objectAtIndex:0];
+    NSString *key = [command.arguments objectAtIndex:1];
+    NSString *value = [command.arguments objectAtIndex:2];
 
     NSDictionary *params = @{ key: value };
-    [Apsalar event:eventName withArgs:params]
+    [Apsalar event:eventName withArgs:params];
 }
 
 @end
