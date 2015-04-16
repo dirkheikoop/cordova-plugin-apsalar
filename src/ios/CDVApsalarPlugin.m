3e08bc4be665098d8e6a082c7c74509364f14c94
Florian Landerl <florian@platogo.com>
2015-04-16 18:06:08 +0200

    Disable automated iOS In-App-Purchase tracking

--- src/ios/CDVApsalarPlugin.m
+++ src/ios/CDVApsalarPlugin.m
@@ -7,6 +7,7 @@
     NSString *apiKey = [command.arguments objectAtIndex:0];
     NSString *apiSecret = [command.arguments objectAtIndex:1];
 
+    [Apsalar setAllowAutoIAPComplete:false];
     [Apsalar startSession:apiKey withKey:apiSecret];
 }
 
